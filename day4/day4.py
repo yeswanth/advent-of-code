58dc2e1bd239b6c0ab1448908c520dd269c0cf73
Yeswanth <swamiyeswanth@gmail.com>
2021-12-04 14:46:47 +0530

    Added day4 solution

--- day4/day4.py
+++ day4/day4.py
@@ -23,7 +23,7 @@ class Puzzle(object):
                 if number == self.matrix[i][j]:
                     self.marked_numbers[i][j] = 1 
         solved,details = self.check_row_column_is_completed()
-        print(self.marked_numbers,solved,details)
+        print(self.marked_numbers,solved,number,details)
         rows_cols = None
         if details:
             rows_cols = self.get_details_row_or_column(details)
@@ -69,10 +69,13 @@ class Puzzle(object):
 split_lines = open('day4.input').read().split("\n\n")
 marked_numbers = split_lines[0]
 matrix_strings = split_lines[1:]
+
+
+# Solution for te first part 
+"""
 matrix_objects = []
 for i in matrix_strings:
     matrix_objects.append(Puzzle(convert_string_to_matrix(i)))
-
 result_found = False
 result_matrix = None
 result_details = None
@@ -91,5 +94,41 @@ for i in marked_numbers.split(","):
 
 
 matrix_unmarked_sum = matr.get_sum_unmarked()
+solution_first = matrix_unmarked_sum*result_number
+"""
 
+# Solution for the second part 
+matrix_objects = []
+result_found = False
+result_matrix = None 
+result_details = None 
+result_number = -1
+for i in matrix_strings:
+    matrix_objects.append(Puzzle(convert_string_to_matrix(i)))
+
+matrixes_found = [False for i in range(len(matrix_objects))]
+
+for no in marked_numbers.split(","):
+    for index,matr in enumerate(matrix_objects):
+        if matrixes_found[index] == True:
+            continue
+        result, details = matr.mark_number(int(no))
+        if result == True:
+            no_of_false = 0
+            for found in matrixes_found:
+                if not found:
+                    no_of_false = no_of_false + 1
+            if no_of_false > 1:
+                matrixes_found[index] = True
+            else:
+                result_found = True
+                result_matrix = matr 
+                result_details = details 
+                result_number = int(no)
+    if result_found:
+            break
+        
+matrix_unmarked_sum = result_matrix.get_sum_unmarked()
 print(matrix_unmarked_sum*result_number)
+
+
