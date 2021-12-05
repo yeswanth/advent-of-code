53cdbca52071e4338c414013ccdb028c79c920c2
Yeswanth <swamiyeswanth@gmail.com>
2021-12-05 12:34:38 +0530

    Added second part of the puzzle

--- day5/day5.py
+++ day5/day5.py
@@ -1,6 +1,7 @@
 lines_parse = open('day5.input').read().split('\n')[:-1]
 lines = []
 horizontal_vertical_lines = []
+diagonal_lines = []
 max_rows,max_cols = -1,-1
 
 for line in lines_parse:
@@ -15,6 +16,8 @@ for line in lines:
     start,end = line 
     if start[0] == end[0] or start[1] == end[1]:
         horizontal_vertical_lines.append(line)
+    else:
+        diagonal_lines.append(line)
     if start[0] > max_rows:
         max_rows = start[0]
     if end[0] > max_rows:
@@ -51,12 +54,40 @@ for line in horizontal_vertical_lines:
             for j in range(start_y,end_y+1):
                 matrix[start_x][j] = matrix[start_x][j] + 1 
 
+# only considering vertical and horizontal lines 
 total_counts_greater_than_2 = 0
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
         if matrix[i][j] >= 2:
             total_counts_greater_than_2 = total_counts_greater_than_2 + 1 
 
-print(total_counts_greater_than_2)
+# print(total_counts_greater_than_2)
 
+# Consider diagonal lines 
+for line in diagonal_lines:
+    start,end = line 
+    start_x,start_y = start
+    end_x,end_y = end 
+    x_diff = 0
+    y_diff = 0
+    if start_x > end_x:
+        x_diff = -1 
+    else:
+        x_diff = 1 
+    if start_y > end_y:
+        y_diff = -1
+    else:
+        y_diff = 1
 
+    for i in range(0,max(start_x-end_x,end_x-start_x)+1):
+        point_x = start_x + x_diff * i 
+        point_y = start_y + y_diff * i 
+        matrix[point_x][point_y] = matrix[point_x][point_y] + 1 
+
+total_counts = 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        if matrix[i][j] >= 2:
+            total_counts= total_counts+ 1
+print(total_counts)
+ 
