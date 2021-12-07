daa6ee206aa2c1d955b784ff29c358868f5c84c1
Yeswanth <swamiyeswanth@gmail.com>
2021-12-07 15:14:23 +0530

    part 2

--- day7/day7.py
+++ day7/day7.py
@@ -3,11 +3,19 @@ f = open('day7.input').read().strip()
 positions = [int(i) for i in f.split(',')]
 
 def fuel_to_final_position(positions,final_position):
-    pass
     fuel = 0
     for position in positions:
         fuel = fuel + max(position-final_position, final_position-position)
     return fuel 
+
+def fuel_to_final_position_part_b(positions,final_position):
+    fuel = 0
+    for position in positions:
+        diff = abs(position - final_position)
+        fuel = fuel + ((diff*(diff+1))/2)
+    return fuel 
+
+
 
 max_position_possible = max(positions)
 
@@ -17,4 +25,13 @@ for position in range(max_position_possible):
     if fuel < min_fuel: 
         min_fuel = fuel 
 
-print(min_fuel)
+# print(min_fuel)
+
+# Part B 
+min_fuel = sys.maxsize 
+for position in range(max_position_possible):
+    fuel = fuel_to_final_position_part_b(positions,position)
+    if fuel < min_fuel:
+        min_fuel = fuel 
+
+print(int(min_fuel))
